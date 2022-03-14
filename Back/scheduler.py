3cea1ad57faaf7ce6569770e99c380c6eebf0113
Juan Cocina <juan_cocina@csu.fullerton.edu>
2022-03-14 15:59:31 -0700

    Changed, deleted, fixed, and finished

--- Back/scheduler.py
+++ Back/scheduler.py
@@ -7,99 +7,38 @@ import schedule
 import time
 import datetime
 from datetime import datetime
-import json
+# import json
 import requests
-import hug
-
-# Global Variables 
-list_of_tasks = []
 
 #
-#   Class Task dedicated to creating objects called tasks
-#
-class Task:
-    def __init__(self, task_id, phone_number, task_description, date, time):
-        self.id = task_id
-        self.number = phone_number
-        self.task_desc = task_description
-        self.date = date
-        self.time = time
-
-        # parse the date into an English day of the week
-        y, m, d = (int(x) for x in date.split('-'))  
-        day = datetime.date(y, m, d)
-        self.day = day.strftime("%A")
-        
-        # Assign schedule based on the result
-        '''
-        if self.day == 'Monday':
-            self.schedule = schedule.every().monday.at(self.time).do(sendOut(self))
-        elif self.day == 'Tuesday':
-            self.schedule = schedule.every().tuesday.at(self.time).do(sendOut(self))
-        elif self.day == 'Wednesday':
-            self.schedule = schedule.every().wednesday.at(self.time).do(sendOut(self))
-        elif self.day == 'Thursday':
-            self.schedule = schedule.every().thursday.at(self.time).do(sendOut(self))
-        elif self.day == 'Friday':
-            self.schedule = schedule.every().friday.at(self.time).do(sendOut(self))
-        elif self.day == 'Saturday':
-            self.schedule = schedule.every().saturday.at(self.time).do(sendOut(self))
-        elif self.day == 'Sunday':
-            self.schedule = schedule.every().sunday.at(self.time).do(sendOut(self))
-        '''
-#
-#   End of Task class
-#
-
-#
-#   Job dedicated to scanning/retrieving tasks from the BD
+#   Job dedicated to scanning tasks in the DB & checking if they should be send
 #
 def scanDB():
     retrieved_tasks = requests.get(f'http://localhost:8000/retrieveAllTasks/')
-    result = retrieved_tasks.json()
-    print(result)
-    tasks = []  #  list tasks from results
-
-    # parse from JSON and create Task objects for x in results
-    for x in result:
-        print('test')
-
-#
-#   Function dedicated to sending the signal to pythonsms.py, to send the text
-#
-def sendOut():
     now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
+    current_time = now.strftime("%I:%M%p") 
+    current_date = now.strftime("%Y-%m-%d")
 
-    for x in list_of_tasks:
-        if x.time == current_time:
-            payload = {
-                'id': x.id,
-                'phone_number': x.phone_number,
-                'task_description': x.task_description,
-            }
-            requests.post(f'http://localhost:8001/sendMessage/', data=payload)
-            list_of_tasks.remove(x)
+    tasks = retrieved_tasks.json()
+    
+    if retrieved_tasks.status_code == 200:
+        for x in tasks:  #  access the tasks
+            for y in tasks[x]:  #  iterate through the tasks + we can access specific variables now
+                if y['time'] == current_time and y['date'] == current_date:
+                    # print(y) uncomment this line to see the task print in terminal
 
-
-#
-#   Testing class
-#
-def test1():
-    #t1 = Task(2, '+13235097821', 'this is a test', '2022-3-8', '16:38')
-    #print(t1.id)
-    #print(t1.day)
-    #print(t1.time)
-    #print(t1.schedule)
-    now = datetime.now()
-
-    current_time = now.strftime("%H:%M:%S") 
-    print(current_time)
+                    #  send text
+                    payload = {
+                        'id': y['id'],
+                        'phone_number': y['phone_number'],
+                        'task_description': y['task_description']
+                    }
+                    response = requests.post('http://localhost:8001/sendMessage/', data=payload)
+    else:
+        print({"Status Code": "500", "Message": "Could not retreive from the DB..."})
 
 #   List of current Jobs
-schedule.every(10).seconds.do(test1)
-# check to see if texts need to be sent every minute...
-# schedule.every(60).seconds.do(sendOut)
+schedule.every().minute.at(":00").do(scanDB)
 
 #   Loop to keep the script running and scheduled jobs going
 while True:
