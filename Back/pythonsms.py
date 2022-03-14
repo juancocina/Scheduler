b1c0aa49df21a285de7dc9b925d0ec5232a27eb0
Juan Cocina <juan_cocina@csu.fullerton.edu>
2022-03-14 15:58:40 -0700

    Changed, deleted, fixed, and finished

--- Back/pythonsms.py
+++ Back/pythonsms.py
@@ -19,36 +19,17 @@ myNumber = keys.phone()  # this will have be adjusted to take different phone nu
 
 client = Client(account_sid, auth_token)
 
-
-#
-#  Testing the sending function
-#  ...Every time lines 21-25 are compiled, a message is sent to my phone
-#  They'll be commented out to prevent spam to my phone...
-
-message = client.messages.create(
-    from_='+16066378278',
-    body='this is a test',
-    to=myNumber
-)
-print(message.sid)
-
-#
-#  Route
-#
-@hug.get('/')
-def landing():
-    return {"Status Code": "200", "Message": "PythonSMS API is up and running!"}
-
 #
 #  Sending a message function
 #
 @hug.post('/sendMessage')
-def send(request, id: hug.types.text, phone_number: hug.types.number, task_description: hug.types.text):
-    phone_number = request.params.get("phone_number")
-    task_description = request.params.get("task_description")
+def send(body):
+    task_description = body['task_description']
+    phone_number = body['phone_number']
+    id = body['id']
 
-    message = client.message.create(
-        from_='+16066378278',
+    message = client.messages.create(
+        from_= myNumber,
         body=task_description,
         to=phone_number
     )
@@ -56,9 +37,9 @@ def send(request, id: hug.types.text, phone_number: hug.types.number, task_descr
     if(message.sid):
         #  Going to have to come back and test this line when the time comes
         #  (Its supposed to delete tasks from the DB)
-        response = requests.delete(f'http://localhost:8000/deleteTask/', data={id})
+        response = requests.delete(f'http://localhost:8000/deleteTask/{id}')
         
         # send a message to tasks.py to delete the task that was just sent out
-        return {"Status:": str(message.sid)}
+        return response.json()
     else:
-        return {"Error": "Message Could Not be Sent"}
+        return response.json()
